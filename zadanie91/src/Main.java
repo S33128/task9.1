7a5fa1347972d2f7ac552ac2a955f5ce98b25c05
s33128 <s33128@pjwstk.edu.pl>
2025-06-17 16:33:48 +0200

    koniec

--- zadanie91/src/Main.java
+++ zadanie91/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        ArrayList<Car> cars = new ArrayList<Car>();
+        cars.add(new Car("toyota", 2010));
+        cars.add(new Car("bmw", 2003));
+        cars.add(new Car("mitsubishi", 2020));
+        cars.add(new Car("milk", 2007));
+        cars.add(new Car("polonez", 1993));
+        cars.add(new Car("skoda", 2015));
+        cars.add(new Car("honda", 2006));
+        cars.add(new Car("mercedes", 1990));
+        cars.add(new Car("volksvagen", 2004));
+        cars.add(new Car("ford", 1992));
+
+        Collections.sort(cars);
+        for (Car car : cars){
+            System.out.println(car);
+        }
     }
 }
